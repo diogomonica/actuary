4f6b9151091022b50a66e9d52f923285380b7a5a
Paris Zoumpouloglou <pariszoump@gmail.com>
2016-03-10 19:29:03 +0200

    More Container Runtime checks.
    Signed-off-by: Paris Zoumpouloglou <pariszoump@gmail.com>

--- audit/container/runtime/runtime.go
+++ audit/container/runtime/runtime.go
@@ -7,12 +7,22 @@ import (
 	"github.com/docker/engine-api/types"
 	// "github.com/docker/engine-api/types/container"
 	"log"
+	"strings"
+	"strconv"
 )
 
 var checks = map[string]audit.Check{
 	"apparmor_profile": CheckAppArmor,
 	"selinux_options": CheckSELinux,
 	"single_process": CheckSingleMainProcess,
+	"kernel_capabilities": CheckKernelCapabilities,
+	"privileged_containers": CheckPrivContainers,
+	"sensitive_dirs": CheckSensitiveDirs,
+	"ssh_running": CheckSshRunning,
+	"privileged_ports": CheckPrivilegedPorts,
+	"needed_ports": CheckNeededPorts,
+
+
 }
 
 func GetAuditDefinitions() map[string]audit.Check {
@@ -31,7 +41,6 @@ func CheckAppArmor(client *client.Client) audit.Result {
 		log.Printf("Unable to get container list")
 		return res
 	}
-
 	if len(containers) == 0 {
 		res.Status = "INFO"
 		res.Output = "No running containers"
@@ -70,7 +79,6 @@ func CheckSELinux(client *client.Client) audit.Result {
 		log.Printf("Unable to get container list")
 		return res
 	}
-
 	if len(containers) == 0 {
 		res.Status = "INFO"
 		res.Output = "No running containers"
@@ -105,7 +113,6 @@ func CheckSingleMainProcess(client *client.Client) audit.Result {
 		log.Printf("Unable to get container list")
 		return res
 	}
-
 	if len(containers) == 0 {
 		res.Status = "INFO"
 		res.Output = "No running containers"
@@ -129,6 +136,220 @@ func CheckSingleMainProcess(client *client.Client) audit.Result {
 		res.Status = "WARN"
 		res.Output = fmt.Sprintf("Containers running more than one main process: %s", badContainers)
 	}
+
+	return res
+}
+
+func CheckKernelCapabilities(client *client.Client) audit.Result {
+	var res audit.Result
+	var badContainers []string
+	res.Name = "5.4 Restrict Linux Kernel Capabilities within containers"
+	options := types.ContainerListOptions{All: false}
+	containers, err := client.ContainerList(options)
+	if err != nil {
+		log.Printf("Unable to get container list")
+		return res
+	}
+	if len(containers) == 0 {
+		res.Status = "INFO"
+		res.Output = "No running containers"
+		return res
+	}
+
+	for _, container := range containers {
+		info, _ := client.ContainerInspect(container.ID)
+		kernelCap := info.HostConfig.CapAdd
+		if kernelCap != nil {
+			badContainers = append(badContainers, container.ID)
+		}
+	}
+
+	if len(badContainers) == 0 {
+		res.Status = "PASS"
+	} else {
+		res.Status = "WARN"
+		res.Output = fmt.Sprintf("Containers running with added kernel capabilities: %s", badContainers)
+	}
+
+	return res
+}
+
+func CheckPrivContainers(client *client.Client) audit.Result {
+	var res audit.Result
+	var badContainers []string
+	res.Name = "5.5 Do not use privileged containers"
+	options := types.ContainerListOptions{All: false}
+	containers, err := client.ContainerList(options)
+	if err != nil {
+		log.Printf("Unable to get container list")
+		return res
+	}
+	if len(containers) == 0 {
+		res.Status = "INFO"
+		res.Output = "No running containers"
+		return res
+	}
+
+	for _, container := range containers {
+		info, _ := client.ContainerInspect(container.ID)
+		privileged := info.HostConfig.Privileged
+		if privileged == true {
+			badContainers = append(badContainers, container.ID)
+		}
+	}
+
+	if len(badContainers) == 0 {
+		res.Status = "PASS"
+	} else {
+		res.Status = "WARN"
+		res.Output = fmt.Sprintf("Privileged containers found: %s", badContainers)
+	}
+
+	return res
+}
+
+func CheckSensitiveDirs(client *client.Client) audit.Result {
+	var res audit.Result
+	var badContainers []string
+	res.Name = "5.6 Do not mount sensitive host system directories on containers "
+	options := types.ContainerListOptions{All: false}
+	sensitiveDirs := []string{"/dev", "/etc", "/lib", "/proc", "/sys", "/usr"}
+	containers, err := client.ContainerList(options)
+	if err != nil {
+		log.Printf("Unable to get container list")
+		return res
+	}
+	if len(containers) == 0 {
+		res.Status = "INFO"
+		res.Output = "No running containers"
+		return res
+	}
+
+	for _, container := range containers {
+		info, _ := client.ContainerInspect(container.ID)
+		mounts := info.Mounts
+		for _, mount := range mounts {
+			for _, dir := range sensitiveDirs {
+				if strings.HasPrefix(mount.Source,dir) && mount.RW == true {
+					badContainers = append(badContainers, container.ID)
+				}
+			}
+		}
+	}
+
+	if len(badContainers) == 0 {
+		res.Status = "PASS"
+	} else {
+		res.Status = "WARN"
+		res.Output = fmt.Sprintf("Sensitive directories mounted on containers: %s", badContainers)
+	}
+
+	return res
+}
+
+func CheckSshRunning(client *client.Client) audit.Result {
+	var res audit.Result
+	var badContainers []string
+	res.Name = "5.7 Do not run ssh within containers"
+	options := types.ContainerListOptions{All: false}
+	containers, err := client.ContainerList(options)
+	if err != nil {
+		log.Printf("Unable to get container list")
+		return res
+	}
+	if len(containers) == 0 {
+		res.Status = "INFO"
+		res.Output = "No running containers"
+		return res
+	}
+
+	for _, container := range containers {
+		procs, _ := client.ContainerTop(container.ID, []string{})
+		//proc fields are [UID PID PPID C STIME TTY TIME CMD]
+		for _, proc :=range procs.Processes {
+			procname := proc[7]
+			if strings.Contains(procname,"ssh") {
+				badContainers = append(badContainers, container.ID)
+			}
+		}
+	}
+	if len(badContainers) == 0 {
+		res.Status = "PASS"
+	} else {
+		res.Status = "WARN"
+		res.Output = fmt.Sprintf("Containers running SSH service: %s", badContainers)
+	}
+
+	return res
+}
+
+func CheckPrivilegedPorts(client *client.Client) audit.Result {
+	var res audit.Result
+	var badContainers []string
+	res.Name = "5.8 Do not map privileged ports within containers"
+	options := types.ContainerListOptions{All: false}
+	containers, err := client.ContainerList(options)
+	if err != nil {
+		log.Printf("Unable to get container list")
+		return res
+	}
+	if len(containers) == 0 {
+		res.Status = "INFO"
+		res.Output = "No running containers"
+		return res
+	}
+
+	for _, container := range containers {
+		info, _ := client.ContainerInspect(container.ID)
+		ports := info.NetworkSettings.Ports
+		for _, port := range ports {
+			for _, portmap := range port {
+				hostPort, _ := strconv.Atoi(portmap.HostPort)
+				if  hostPort < 1024 {
+					badContainers = append(badContainers, container.ID)
+				}
+			}
+		}
+	}
+
+	if len(badContainers) == 0 {
+		res.Status = "PASS"
+	} else {
+		res.Status = "WARN"
+		res.Output = fmt.Sprintf("Containers with mapped privileged ports: %s", badContainers)
+	}
+
+	return res
+}
+
+func CheckNeededPorts(client *client.Client) audit.Result {
+	var res audit.Result
+	var containerPort map[string][]string
+	containerPort = make(map[string][]string)
+
+	res.Name = "5.9 Open only needed ports on container"
+	options := types.ContainerListOptions{All: false}
+	containers, err := client.ContainerList(options)
+	if err != nil {
+		log.Printf("Unable to get container list")
+		return res
+	}
+
+	if len(containers) == 0 {
+		res.Status = "INFO"
+		res.Output = "No running containers"
+		return res
+	}
+
+	for _, container := range containers {
+		info, _ := client.ContainerInspect(container.ID)
+		ports := info.NetworkSettings.Ports
+		for key, _ := range ports {
+			containerPort[container.ID] = append(containerPort[container.ID], string(key))
+		}
+	}
+		res.Status = "INFO"
+		res.Output = fmt.Sprintf("Containers with mapped privileged ports: %v \n", containerPort)
 
 	return res
 }
